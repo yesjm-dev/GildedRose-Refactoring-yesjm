4448fe2333b18a379d864246eccaf6829728a79a
노정민 <48047620+yesjm-dev@users.noreply.github.com>
2023-09-18 23:04:02 +0900

    Initial commit

---
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void Foo()
+    {
+        IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.Equal("fixme", items[0].Name);
+    }
+}
