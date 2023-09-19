1c08d1cc66b737cc174600a251d7be6ad4148d65
jeongmin78 <wudals0708@naver.com>
2023-09-20 00:06:29 +0900

    feat: 0~50 사잇값으로 quality를 관리하는 plusQuality, minusQuality 함수 생성 및 적용

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,37 +3,35 @@ package com.gildedrose
 import com.gildedrose.enum.ItemName
 
 class GildedRose(var items: List<Item>) {
-
     fun updateQuality() {
         for (i in items.indices) {
+            if (items[i].name != ItemName.SULFURAS) {
+                items[i].sellIn = items[i].sellIn - 1
+            }
             if (items[i].name != ItemName.AGED_BRIE && items[i].name != ItemName.BACKSTAGE_PASSES) {
                 if (items[i].quality > 0) {
                     if (items[i].name != ItemName.SULFURAS) {
-                        items[i].quality = items[i].quality - 1
+                        items[i].quality = minusQuality(items[i].quality)
                     }
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                    items[i].quality = plusQuality(items[i].quality)
 
                     if (items[i].name == ItemName.BACKSTAGE_PASSES) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality = plusQuality(items[i].quality)
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality = plusQuality(items[i].quality)
                             }
                         }
                     }
                 }
-            }
-
-            if (items[i].name != ItemName.SULFURAS) {
-                items[i].sellIn = items[i].sellIn - 1
             }
 
             if (items[i].sellIn < 0) {
@@ -41,7 +39,7 @@ class GildedRose(var items: List<Item>) {
                     if (items[i].name != ItemName.BACKSTAGE_PASSES) {
                         if (items[i].quality > 0) {
                             if (items[i].name != ItemName.SULFURAS) {
-                                items[i].quality = items[i].quality - 1
+                                items[i].quality = minusQuality(items[i].quality)
                             }
                         }
                     } else {
@@ -49,10 +47,26 @@ class GildedRose(var items: List<Item>) {
                     }
                 } else {
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                        items[i].quality = plusQuality(items[i].quality)
                     }
                 }
             }
+        }
+    }
+
+    private fun plusQuality(quality: Int): Int {
+        return if (quality in 1..49) {
+            quality + 1
+        } else {
+            quality
+        }
+    }
+
+    private fun minusQuality(quality: Int): Int {
+        return if (quality <= 0) {
+            quality
+        } else {
+            quality - 1
         }
     }
 
