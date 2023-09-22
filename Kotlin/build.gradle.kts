93f55ab267edcf8075c4e239dd8680ab2dd3ae03
jeongmin78 <wudals0708@naver.com>
2023-09-22 23:15:53 +0900

    chore: 테스트코드 실행을 위해 jvm 버전 변경

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,29 +1,29 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.7.22"
 }
 
 group = "com.gildedrose"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation(kotlin("stdlib"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
-	useJUnitPlatform()
-	testLogging {
-		events("passed", "skipped", "failed")
-	}
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
 // config JVM target to 1.8 for kotlin compilation tasks
 tasks.withType<KotlinCompile>().configureEach {
-	kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "11"
 }
