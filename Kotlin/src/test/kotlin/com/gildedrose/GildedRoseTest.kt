93d1a65e0ea01a8a9da7917e4f93dc6afb3a1213
jeongmin78 <wudals0708@naver.com>
2023-09-23 23:35:26 +0900

    test: Backstage passes는 sellIn이 0인 경우 테스트 코드 추가, 테스트 항목 추가

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -16,20 +16,29 @@ internal class GildedRoseTest {
 
     @Test
     fun `sulfuras를 제외한 Item의 Quality는 50을 초과하지 않는다`() {
-        val items = listOf(Item("Aged Brie", 0, 50))
+        val items = listOf(
+            Item("Aged Brie", 0, 50),
+            Item("Elixir of the Mongoose", 10, 50)
+        )
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals(50, app.items[0].quality)
             .also { println("quality: ${app.items[0].quality}") }
-    }
+        assertEquals(49, app.items[1].quality)
+            .also { println("quality: ${app.items[1].quality}") }    }
 
     @Test
     fun `Item의 SellIn이 0보다 작거나 같으면 Quality는 2배로 떨어진다`() {
-        val items = listOf(Item("Elixir of the Mongoose", 0, 20))
+        val items = listOf(
+            Item("Elixir of the Mongoose", 0, 20),
+            Item("Elixir of the Mongoose", -10, 20),
+        )
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals(18, app.items[0].quality)
             .also { println("quality: ${app.items[0].quality}") }
+        assertEquals(18, app.items[1].quality)
+            .also { println("quality: ${app.items[1].quality}") }
     }
 
     @Test
@@ -52,28 +61,52 @@ internal class GildedRoseTest {
 
     @Test
     fun `Backstage passes는 sellIn이 10~ 인 경우 Quality 값이 1 올라간다`() {
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 15, 20))
+        val items = listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 30, 20)
+        )
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals(21, app.items[0].quality)
             .also { println("quality: ${app.items[0].quality}") }
+        assertEquals(21, app.items[1].quality)
+            .also { println("quality: ${app.items[1].quality}") }
     }
 
     @Test
-    fun `Backstage passes는 sellIn이 5~10 인 경우 Quality 값이 2 올라간다`() {
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 49))
+    fun `Backstage passes는 sellIn이 6~10 인 경우 Quality 값이 2 올라간다`() {
+        val items = listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 6, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)
+        )
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals(51, app.items[0].quality)
             .also { println("quality: ${app.items[0].quality}") }
+        assertEquals(51, app.items[1].quality)
+            .also { println("quality: ${app.items[1].quality}") }
     }
 
     @Test
-    fun `Backstage passes는 sellIn이 0~5 인 경우 Quality 값이 2 올라간다`() {
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),)
+    fun `Backstage passes는 sellIn이 1~5 인 경우 Quality 값이 3 올라간다`() {
+        val items = listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 49),
+        )
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals(52, app.items[0].quality)
+            .also { println("quality: ${app.items[0].quality}") }
+        assertEquals(52, app.items[1].quality)
+            .also { println("quality: ${app.items[1].quality}") }
+    }
+
+    @Test
+    fun `Backstage passes는 sellIn이 0인 경우 Quality 값이 0이 된다`() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 49))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
             .also { println("quality: ${app.items[0].quality}") }
     }
 
